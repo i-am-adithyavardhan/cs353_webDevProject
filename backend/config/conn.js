3a7a09f265edf6620573a5273a2e08f019f5f3cc
i-am-adithyavardhan <98834951+i-am-adithyavardhan@users.noreply.github.com>
2023-05-13 15:09:23 +0530

    Update conn.js

--- backend/config/conn.js
+++ backend/config/conn.js
@@ -4,7 +4,7 @@ const db = async () => {
   const conn = await mongoose.connect(
     // "mongodb+srv://saivikas:Vikas%402003@mongodb-demo.21lkg2i.mongodb.net/?retryWrites=true&w=majority"
     // "mongodb+srv://saivikas:Vikas%402003@mongodb-demo.21lkg2i.mongodb.net/BlogProjectdb?retryWrites=true&w=majority"
-    "mongodb+srv://adithya:adithya@cluster0.ojwfqcs.mongodb.net/?retryWrites=true&w=majority"
+    ""
   );
   console.log("Db connected");
 };
